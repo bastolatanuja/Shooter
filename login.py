f527055a368c05aa13a3a13f7634c567b844caf9
bastolatanuja <tanujabastola143@gmail.com>
2021-08-20 22:43:01 +0545

    commit

--- login.py
+++ login.py
@@ -10,8 +10,8 @@ root.resizable(0,0)
 root.geometry("637x688")
 root.resizable(0,0)
 pic=Image.open('Login.png')
-ren=ImageTk.PhotoImage(pic)
-img=Label(root,image=ren)
+renr=ImageTk.PhotoImage(pic)
+img=Label(root,image=renr)
 img.place(x=0,y=0)
 
 def goto_homepage():
@@ -20,28 +20,33 @@ def goto_homepage():
 
 
 
-std_id=Entry(root,bd=10,width=21,relief=FLAT, font=('arial',14,'bold'), bg='#00437c', fg='turquoise3')
-std_id.place(x=260,y=178)
 
-last=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-last.place(x=260,y=236)
+load=Image.open("id icon.png")
+render=ImageTk.PhotoImage(load)
+img1=Label(root,image=render,bd=4)
+img1.place(x=165,y=292)
 
-email=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-email.place(x=260,y=292)
+load=Image.open("password icon.png")
+ren=ImageTk.PhotoImage(load)
+img2=Label(root,image=ren,bd=4)
+img2.place(x=165,y=370)
 
-
-phone=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-phone.place(x=260,y=350)
-
-
-password=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-password.place(x=260,y=407)
+login_img=Image.open("loginbtn.png")
+login_img=ImageTk.PhotoImage(login_img)
 
 
-c_password=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-c_password.place(x=260,y=465)
 
-submit_btn=Button(root,text="submit",width=27,height=2,font=("arial",14,'bold'),fg='white',bg='#00437c',relief=FLAT,activebackground='#00437c',command=goto_homepage)
-submit_btn.place(x=157,y=586)
+std_id=Entry(root,bd=13,width=21,relief=FLAT, font=('arial',14,'bold'), bg='#00437c', fg='turquoise3',highlightthickness=3)
+std_id.config(highlightbackground="white",highlightcolor="white")
+std_id.place(x=220,y=370)
+
+last=Entry(root,bd=13,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white',highlightthickness=3)
+std_id.config(highlightbackground="white",highlightcolor="white")
+last.place(x=220,y=292)
+
+
+
+login_btn=Button(root,image=login_img,width=287,height=53,borderwidth=0,font=("arial",14,'bold'),command=goto_homepage)
+login_btn.place(x=200,y=510)
 
 root.mainloop()
--- signuppage.py
+++ signuppage.py
@@ -18,30 +18,38 @@ def goto_homepage():
     root.destroy()
     os.system('python home.py')
 
+submit_img=Image.open("submit.png")
+submit_img=ImageTk.PhotoImage(submit_img)
 
 
-std_id=Entry(root,bd=10,width=21,relief=FLAT, font=('arial',14,'bold'), bg='#00437c', fg='turquoise3')
-std_id.place(x=260,y=178)
-
-last=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-last.place(x=260,y=236)
-
-email=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-email.place(x=260,y=292)
 
 
-phone=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-phone.place(x=260,y=350)
 
 
-password=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
+
+std_id=Entry(root,bd=8,width=21,relief=FLAT, font=('arial',14,'bold'), bg='#385273', fg='turquoise3')
+std_id.place(x=260,y=210)
+
+last=Entry(root,bd=8,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#385273', fg='white')
+last.place(x=260,y=260)
+
+email=Entry(root,bd=8,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
+email.place(x=260,y=310)
+
+
+phone=Entry(root,bd=8,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
+phone.place(x=260,y=360)
+
+
+password=Entry(root,bd=8,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
 password.place(x=260,y=407)
 
 
-c_password=Entry(root,bd=10,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
-c_password.place(x=260,y=465)
+c_password=Entry(root,bd=8,width=21,relief= FLAT, font=('arial',14,'bold'), bg='#00437c', fg='white')
+c_password.place(x=260,y=458)
 
-submit_btn=Button(root,text="submit",width=27,height=2,font=("arial",14,'bold'),fg='white',bg='#00437c',relief=FLAT,activebackground='#00437c',command=goto_homepage)
-submit_btn.place(x=157,y=586)
+sub_btn=Button(root,image=submit_img,borderwidth=0,command=goto_homepage,width=287,height=53,highlightthickness=3)
+sub_btn.place(x=200,y=510)
+
 
 root.mainloop()
